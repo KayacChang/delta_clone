eabc93a01507672b027647b16f84ccaef4ecef07
kayac <egg734631@gmail.com>
2020-09-19 16:46:49 +0800

    Updates

---
+++ precache-manifest.108c775086502e8dab9e7c42e0071dbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4ef63c68ba8e609631fde6bb82b9b9",
+    "url": "/delta_clone/index.html"
+  },
+  {
+    "revision": "da5cf1e68addcd101f2f",
+    "url": "/delta_clone/static/css/main.aea95503.chunk.css"
+  },
+  {
+    "revision": "0cb550cbddc505a50aa3",
+    "url": "/delta_clone/static/js/2.b57fd2d6.chunk.js"
+  },
+  {
+    "revision": "af3186094d8229f9291a34ca4506cd5d",
+    "url": "/delta_clone/static/js/2.b57fd2d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5cf1e68addcd101f2f",
+    "url": "/delta_clone/static/js/main.fdaacb38.chunk.js"
+  },
+  {
+    "revision": "779ebe0fa403d2e2da12",
+    "url": "/delta_clone/static/js/runtime-main.53ed0d2e.js"
+  }
+]);
